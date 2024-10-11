0a5587b1074d969698dbb206e9af2233f03623a9
Freddy Henderson <hendersonsarah12@gmail.com>
2024-10-11 01:45:08 +0100

    refactored code

--- 5-kyu/The-Hashtag-Generator/generate_hashtag.rb
+++ 5-kyu/The-Hashtag-Generator/generate_hashtag.rb
@@ -1,14 +1,4 @@
 def generate_hashtag(str)
-  new_str = str.split.join(" ")
-  if new_str.empty?
-    return false
-  else
-    new_str.gsub!(/\s{2,}/, " ")
-    hashtag = "##{new_str.split(" ").each {|word| word.capitalize!}.join("")}"
-    if hashtag.length > 140
-      return false
-    else
-      return hashtag
-    end
-  end
+  hashtag = "##{str.split(" ").each {|word| word.capitalize!}.join("")}"
+  (hashtag.length > 140 || hashtag == "#") ? (return false) : (return hashtag)
 end
